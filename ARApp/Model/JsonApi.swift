86ce703cf55875f5b35e8deaed5425fc97d1a6b3
Mike Griffin <mike.griffin@kindredgroup.com>
2023-11-22 09:56:12 +0100

    Use json raw

--- ARApp/Model/JsonApi.swift
+++ ARApp/Model/JsonApi.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class JsonApi {
     func getUserComments(completion:@escaping ([Comments]) -> ()) {
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments") else { return }
+        guard let url = URL(string: "https://raw.githubusercontent.com/kindredgroup/ARApp/master/data.json") else { return }
         
         URLSession.shared.dataTask(with: url) { (data, _, _) in
             let comments = try! JSONDecoder().decode([Comments].self, from: data!)
